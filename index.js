8633f7b7ba3e9238a8f0aeff0157cb11860f58ff
ThangNC <thangnc.mta@gmail.com>
2021-08-21 22:19:54 +0700

    Generate Tag version

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const {Octokit} = require("@octokit/rest");
 
+const getNewReleaseTag = (oldReleaseTag, versionBuild, versionPrefix) => {
+    if (!versionPrefix) versionPrefix = ''
+    if (!oldReleaseTag) {
+        return `${versionPrefix}0.0.0`
+    }
+    oldReleaseTag = oldReleaseTag
+        .replace('v', '')
+        .replace('v.', '')
+        .replace('v-', '')
+        .replace(`${versionPrefix}`, '')
+    const version = oldReleaseTag.split(".").map((x) => Number(x));
+    if (version.length < 1) {
+        return `${versionPrefix}0.0.0`
+    }
+    if (versionBuild === 'major') {
+        version[0] = version[0] + 1
+    } else if (versionBuild === 'minor') {
+        if (version.length < 2) {
+            version[0] = version[0] + 1
+        } else {
+            version[1] = version[1] + 1
+        }
+    } else if (versionBuild === 'patch') {
+        if (version.length < 2) {
+            version[0] = version[0] + 1
+        } else if (version.length < 3) {
+            version[1] = version[1] + 1
+        } else {
+            version[2] = version[2] + 1
+        }
+    } else {
+        version[0] = version[0] + 1
+    }
+    return `${versionPrefix}${version.join('.')}`
+};
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+const generateNextReleaseTag = async () => {
+    try {
+        const githubToken = core.getInput("GITHUB_TOKEN");
+        const versionBuild = core.getInput("VERSION_BUILD");
+        const versionPrefix = core.getInput("PREFIX");
+        const octokit = new Octokit({auth: githubToken})
+        const {owner, repo} = github.context.repo;
+        const response = await octokit.rest.repos.listTags({
+            owner,
+            repo,
+            per_page: 5
+        });
+        console.log(response.data)
+        const oldReleaseTag = response.data ? response.data[0].name : null;
+        const newReleaseTag = getNewReleaseTag(oldReleaseTag, versionBuild, versionPrefix);
+        console.log(`Previous Release Tag: ${oldReleaseTag}`);
+        console.log(`New Release Tag: ${newReleaseTag}`);
+        core.exportVariable("RELEASE_TAG", newReleaseTag);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+};
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run();
+generateNextReleaseTag();
